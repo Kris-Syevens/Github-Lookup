cdd602b41925c01764c8d631badc84a41e3932e1
Kris <kris@syevens.com>
2022-10-02 22:24:04 -0500

    Initial Commit - Application Setup

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
