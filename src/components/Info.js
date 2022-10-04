4f8e3f641f776422fbfb108acd141cf7e570997d
Kris <kris@syevens.com>
2022-10-04 12:51:36 -0500

    Update Commit - Updated Card, User, Followers, and Repos Components

--- src/components/Info.js
+++ src/components/Info.js
@@ -40,7 +40,7 @@ const UserInfo = () => {
   ];
 
   return (
-    <Wrapper>
+    <Wrapper className="section-center">
       {items.map((item) => {
         return <Item key={item.id} {...item} />;
       })}
