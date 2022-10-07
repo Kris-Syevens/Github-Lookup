0fb0a8c36c14ca08eed7470ef8c12d2ca8a800c4
Kris <kris@syevens.com>
2022-10-06 22:45:37 -0500

    Update Commit - Updated Search/context/axios config

--- src/context/context.js
+++ src/context/context.js
@@ -13,8 +13,80 @@ const GithubProvider = ({ children }) => {
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
 
+  const [requests, setRequests] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [error, setError] = useState({ show: false, msg: "" });
+
+  // Check rate
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequests(remaining);
+        if (remaining === 0) {
+          toggleError(true, "Sorry, you have exceeded your hourly rate!");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // Error Handling
+
+  function toggleError(show = false, msg = "") {
+    setError({ show, msg });
+  }
+
+  // Search Github User
+
+  const searchGithubUser = async (user) => {
+    toggleError();
+    setIsLoading(true);
+    try {
+      const response = await axios(`${rootUrl}/users/${user}`);
+      if (response) {
+        setGithubUser(response.data);
+        const { login, followers_url } = response.data;
+
+        await Promise.allSettled([
+          axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+          axios(`${followers_url}?per_page=100`),
+        ]).then((results) => {
+          const [repos, followers] = results;
+          const status = "fulfilled";
+          if (repos.status === status) {
+            setRepos(repos.value.data);
+          }
+          if (followers.status === status) {
+            setFollowers(followers.value.data);
+          }
+        });
+      }
+      checkRequests();
+      setIsLoading(false);
+    } catch (error) {
+      toggleError(true, "there is no user with entered username.");
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(checkRequests, []);
+
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+        isLoading,
+      }}
+    >
       {children}
     </GithubContext.Provider>
   );
